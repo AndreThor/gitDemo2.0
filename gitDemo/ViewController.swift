1222805683291800a2b644f639129f4f87ff91ad
AndreThor <andreasthorsen10@gmail.com>
2017-12-07 17:36:56 +0000

    Initial Commit

---
+++ gitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemo
+//
+//  Created by Andreas Thorsen on 07/12/2017.
+//  Copyright © 2017 Andreas Thorsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
